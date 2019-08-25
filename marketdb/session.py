517f6119ce70a6929eda4821b1fada93a8a266c9
Dant86 <vedantdpathak@iCloud.com>
2019-08-25 14:02:37 +0100

    skeleton done

--- marketdb/session.py
+++ marketdb/session.py
@@ -1,10 +1,9 @@
 from dotenv import load_dotenv
 from os import getenv
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-def get_params(dbname, env_path='.env'):
+def get_session(env_path='.env'):
     load_dotenv(dotenv_path=env_path)
     sqltype = getenv("SQL_TYPE")
     port = getenv("DB_PORT")
@@ -15,5 +14,4 @@ def get_params(dbname, env_path='.env'):
     url = f'{sqltype}://{usr}:{pwrd}@{host}:{port}/{name}'
     engine = create_engine(url)
     Session = sessionmaker(bind=engine)
-    Base = declarative_base()
-    return Session, base
+    return Session
